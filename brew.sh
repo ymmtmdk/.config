723bd9c026e5411413276c7a2e4592bfee86d9b7
ymmtmdk <ymmtmdk@gmail.com>
2024-02-10 23:31:05 +0900

    NC

---
+++ brew.sh
@@ -0,0 +1,4 @@
+brew install starship ruby neovim fish peco karabiner-elements google-chrome brave-browser iterm2 visual-studio-code git-credential-manager
+
+code --install-extension vscodevim.vim
+code --install-extension GitHub.copilot
---
+++ fisher.sh
@@ -0,0 +1,3 @@
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+fisher install ymmtmdk/autocdz ymmtmdk/peco_select_history_with_c homebrew/cask-fonts/font-roboto-mono-nerd-font
