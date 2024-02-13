cc0fa6799997a65a14d168de85cc56c171bb35a6
ymmtmdk <ymmtmdk@gmail.com>
2024-02-13 13:12:23 +0900

    NC

--- fisher.sh
+++ fisher.sh
@@ -1,3 +1,6 @@
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
-fisher install ymmtmdk/autocdz ymmtmdk/peco_select_history_with_c homebrew/cask-fonts/font-roboto-mono-nerd-font
+fisher install ymmtmdk/autocdz ymmtmdk/peco_select_history_with_c jorgebucaran/nvm.fishe
+nvm install latest
+set --universal nvm_default_version latest
+
