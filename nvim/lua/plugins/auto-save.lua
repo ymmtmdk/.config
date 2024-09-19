674a5cdce7e3c91dc5eacc552faa55ee1810b098
ymmtmdk <ymmtmdk@gmail.com>
2024-09-20 06:18:48 +0900

    NC

--- nvim/lua/plugins/auto-save.lua
+++ nvim/lua/plugins/auto-save.lua
@@ -4,7 +4,12 @@ return {
   dev = true,
   config = function()
     require("auto-save").setup {
-      debounce_delay = 2000,
+      -- debounce_delay = 1000,
+      -- trigger_events = { "InsertLeave", "TextChanged", "FocusLost", "TabLeave", "BufLeave" },
     }
   end
 }
+
+
+
+-- aaa aaaa [[[
