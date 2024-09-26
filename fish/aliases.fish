a0a4a78ee6f2e8ca0b34d5e2f8e62120f466b442
ymmtmdk <ymmtmdk@gmail.com>
2024-09-26 21:12:58 +0900

    NC

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,19 +1,20 @@
 alias e='nvim'
 alias r='ruby'
+alias py='python3'
 
 if which hub > /dev/null
   alias g='hub'
 else
   alias g='git'
 end
-alias d='docker'
 
 alias ll='ls -lAh'
 alias df='df -m'
 
-alias bi="bundle install"
-alias bu='bundle update'
-alias be='bundle exec'
+# alias bi="bundle install"
+# alias bu='bundle update'
+# alias be='bundle exec'
 
-alias docker-stop-all-ps='docker stop (docker ps -a -q)'
-alias docker-rm-all-ps='docker rm (docker ps -a -q)'
+# alias d='docker'
+# alias docker-stop-all-ps='docker stop (docker ps -a -q)'
+# alias docker-rm-all-ps='docker rm (docker ps -a -q)'
