d6e458f61a29889b1c90e22469cb4a97932d1de8
ymmtmdk <ymmtmdk@gmail.com>
2024-10-17 14:25:50 +0900

    NC

---
+++ setup_linux.sh
@@ -0,0 +1,10 @@
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> ~/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+sudo apt-get install build-essential
+brew install gcc starship ruby neovim fish peco
+
