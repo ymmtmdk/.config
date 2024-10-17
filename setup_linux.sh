4b12dda2ab36207b8945070a066d95c7b4df0c39
ymmtmdk <ymmtmdk@gmail.com>
2024-10-17 14:47:18 +0900

    NC

--- setup_linux.sh
+++ setup_linux.sh
@@ -8,3 +8,19 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt-get install build-essential
 brew install gcc starship ruby neovim fish peco
 
+# $ sudo vim /etc/inputrc
+# # set bell-style none
+# ↓ ↓ ↓
+# set bell-style none
+
+# WINDOWS/<username>\.wslconfig に以下を追記するだけです。
+# .wslconfig
+#[wsl2]
+# dnsTunneling=true
+
+# /etc/wsl.conf
+# [network]
+# generateResolvConf=false
+
+# # edit /etc/resolv.conf
+
