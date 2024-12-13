da8b83c7ea6242d16859ec15fd669ce677576637
ymmtmdk <ymmtmdk@gmail.com>
2024-12-13 17:22:31 +0900

    NC

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -7,6 +7,7 @@ local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 vim.g.mapleader = ';'
+
 -- Modes
 --   normal_mode = 'n',
 --   insert_mode = 'i',
@@ -15,13 +16,22 @@ vim.g.mapleader = ';'
 --   term_mode = 't',
 --   command_mode = 'c',
 
--- Normal --
-keymap("n", "<F13>", "1gt", opts)
-keymap("n", "<F14>", "2gt", opts)
-keymap("n", "<F15>", "3gt", opts)
-keymap("n", "<F16>", "4gt", opts)
-keymap("n", "<F17>", "5gt", opts)
-keymap("n", "<F18>", "6gt", opts)
+if vim.fn.has('mac') > 0 then
+  keymap("n", "<F13>", "1gt", opts)
+  keymap("n", "<F14>", "2gt", opts)
+  keymap("n", "<F15>", "3gt", opts)
+  keymap("n", "<F16>", "4gt", opts)
+  keymap("n", "<F17>", "5gt", opts)
+  keymap("n", "<F18>", "6gt", opts)
+else
+  keymap("n", "<F3>", "1gt", opts)
+  keymap("n", "<F4>", "2gt", opts)
+  keymap("n", "<F5>", "3gt", opts)
+  keymap("n", "<F6>", "4gt", opts)
+  keymap("n", "<F7>", "5gt", opts)
+  keymap("n", "<F8>", "6gt", opts)
+end
+
 keymap("n", "<C-n>", ":tabnew<Space>", {})
 keymap("n", "<C-S-f>", ":NvimTreeToggle<cr>", opts)
 
