71077714c2bd8238c91c627fb5b2b36f1237239c
ymmtmdk <ymmtmdk@gmail.com>
2025-08-22 23:07:55 +0900

    a

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install starship ruby neovim fish peco karabiner-elements google-chrome brave-browser iterm2 visual-studio-code git-credential-manager
+brew install starship mise neovim fish peco karabiner-elements google-chrome iterm2 visual-studio-code git-credential-manager
 
 # vscode
 code --install-extension asvetliakov.vscode-neovim
@@ -10,10 +10,4 @@ defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 # code --install-extension vscodevim.vim
 code --install-extension GitHub.copilot
 
-# fisher
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
-fisher install ymmtmdk/autocdz ymmtmdk/peco_select_history_with_c jorgebucaran/nvm.fishe
-nvm install latest
-fish -c 'set --universal nvm_default_version latest'
 
--- setup_linux2.fish
+++
@@ -1,7 +0,0 @@
-# fisher
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
-fisher install ymmtmdk/autocdz ymmtmdk/peco_select_history_with_c jorgebucaran/nvm.fishe
-nvm install latest
-fish -c 'set --universal nvm_default_version latest'
-
