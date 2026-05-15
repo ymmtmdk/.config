ea386fd277430307aa59aded58c0fec074decbfc
ymmtmdk <ymmtmdk@gmail.com>
2026-05-15 10:23:49 +0900

    style(nvim): set lualine theme to auto for Kimbie Dark

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup {
-      options = { theme = 'nightfly' },
+      options = { theme = 'auto' },
     }
   end
 }
